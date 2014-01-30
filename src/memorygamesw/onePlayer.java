903ca8dcc0859332b9732cf6dd6533d45f35af45
KenandEva <KenandEva@192.168.0.3>
2014-01-29 22:42:16 -0700

    I wasn't able to do the test game. I couldn't figure it out.

--- src/memorygamesw/onePlayer.java
+++ src/memorygamesw/onePlayer.java
@@ -22,31 +22,21 @@ package memorygamesw;
 // The game is a lose if time : t>100
 
 public class onePlayer {
-    int time=30;
-    boolean cards=true;// true if cards matches, false if not.
-    int player;// 1 or 2 user inmput.
- 
-    public double getWinningTime(time){
-                // the player will win the game if the game is finished in 160seconds
-              
-        if ((50<time)&&(cards=true)){
-        System.out.println("you win the the game in" +time+"seconds");
-     }
-       
-        if ((time>50)&&(time<100)&&(card=true)
-        System.out.println("you win the the game in" +time+"seconds");
-    }  
-         // the player will lose the gane if the game isn't finished 
-        //and the time is out
-        if ((time>=160)&&(cars=false)){
-        System.out.println ("You loose!");
-    }
-        public double getWinningScore(score){
-       double score=100-time;// the score is based on time spent
-       //on the game till wining
-       System.out.println("your score is"+score);
+    int startingPoints= 100;
+    int gameMove= 50;
+    boolean cards = true;// true if cards matches, false if not.
+    int score=startingPoints-gameMove;
+    public void getWinningScore(){
+            
+        if (cards==true){
+        System.out.println("you win!score: " +score);
         }
-               
-          }   
+        else if((cards==false) && (gameMove==100)){
+            System.out.println("you loose!");
+                }
+    }
+}
+
+
 
 
