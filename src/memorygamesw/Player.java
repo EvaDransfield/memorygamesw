adfba9e2dbdc1e6550dcdb4e1fc0d5e9da227016
Cope-Michele <michelevcope@gmail.com>
2014-02-01 20:37:34 -0700

    Updated the Paired Programming Assignment- MC

--- src/memorygamesw/Player.java
+++ src/memorygamesw/Player.java
@@ -21,40 +21,21 @@ package memorygamesw;
 
 // Eva's individual assignment lesson 3
 public class Player {
-    double startingPoints=115.00;
-    int gameMove;
-    boolean cards;// true if cards matches, false if not.
-    
-  
-
-    public void setGameMove(int gameMove) {
-        this.gameMove = gameMove;
-    }
-
-    public void setCards(boolean cards) {
-        this.cards = cards;
-    }
-    
-       
-    public void getWinningScore(int gameMove, boolean cards){
-        
-        int score= (int) (startingPoints)-gameMove;// cast double to int
-                    if ((cards==true)&&(gameMove==15)){
-        System.out.println("you win perfect score!: " +score+" points\n");
+    int startingPoints= 115;
+    int gameMove= 134;
+    boolean cards = true;// true if cards matches, false if not.
+    int score=startingPoints-gameMove;
+    public void getWinningScore(){
+            
+        if ((cards==true)&&(gameMove==15)){
+        System.out.println("you win perfect score!: " +score+"points");
         }
         else if  ((cards==true) && (gameMove<115)){
-            System.out.println("you win  "+score+" points\n");
+            System.out.println("you win  "+score+"points");
                 }
-                
-        else if((cards==false) && (gameMove==115)){
-            System.out.println("you loose! "+ score+" points\n");
+        
+        else if((cards==false) && (gameMove>=115)){
+            System.out.println("you loose! "+ score+"points");
                 }
-        else if((cards==true) && (gameMove<=0)){
-            System.out.println("invalid input\n");
-                }
-        else if((cards==false) && (gameMove>115)){
-            System.out.println("invalid input\n");
-                }
-                    
     }
 }
