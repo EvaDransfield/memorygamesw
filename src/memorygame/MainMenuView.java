8069479d0ed9b064738d3889eb267c962f14eb59
Mpianatra <Mpianatra@192.168.0.2>
2014-02-05 23:07:06 -0700

    Individual  Main MenuControl

---
+++ src/memorygame/MainMenuView.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memorygame;
+import java.util.Scanner;
+
+
+
+
+/**
+ *
+ * @author KenandEva
+ */
+// first appears to the screen
+// this is what you see when first open the program
+// the main menu will have: Easy, Normal, and hard option
+public class MainMenuView {
+    
+    
+       // create frame for the window,
+    // I use static final because those varibale will never gonna change 
+    
+    public static final int width=320;// can be changed 
+    
+    public static final int height= width/12*9;
+    
+    public static final int scale = 2;
+    
+    private static final String[][] menuItems = {
+        {"1", "One player game"},
+        {"2", "Two player game"},
+        {"H", "Help"},
+        {"X", "Exit Memory Game"},
+    }; 
+    
+  
+   MainMenuControl mainMenuControl=new MainMenuControl();
+   public MainMenuView(){
+       
+   }
+   public void getInput()   {
+       String command;
+       Scanner inFile=new Scanner(System.in);
+       do {
+           this.display();//display the menu 
+           
+           // get command entered
+           command= inFile.nextLine();
+           command=command.trim().toUpperCase;
+           
+           
+           switch (command) {
+               case "1":
+                    this.mainMenuControl.startGame(1);
+                    break;
+                case "2":
+                    this.mainMenuControl.startGame(2);
+                    break;
+                case "X":
+                    break;
+                default: 
+                    new MemoryGameError().displayError("Invalid command. Please enter a valid command.");
+                 continue;                    
+            }
+        } while (!command.equals("X"));
+
+        return;
+   }
+}
+        
+       
+   
+    
+
